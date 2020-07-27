f2e16d5ce2ee7dab6e6859bb1f2b5e3ef4bc0966
nana-4 <hnmaigo@gmail.com>
2020-07-27 12:11:30 +0900

    scripts: Carefully check if inkscape exists
    This prevents the scripts from exiting if rendersvg is installed but
    inkscape is not.
    
    And make sure inkscape is available when rendersvg is unavailable.

--- src/gtk-2.0/render-asset.sh
+++ src/gtk-2.0/render-asset.sh
@@ -5,6 +5,16 @@ RENDER_SVG="$(command -v rendersvg)" || true
 INKSCAPE="$(command -v inkscape)" || true
 OPTIPNG="$(command -v optipng)" || true
 
+if [[ -n "${INKSCAPE}" ]]; then
+  if "$INKSCAPE" --help | grep -e "--export-filename" > /dev/null; then
+    EXPORT_FILE_OPTION="--export-filename"
+  elif "$INKSCAPE" --help | grep -e "--export-file" > /dev/null; then
+    EXPORT_FILE_OPTION="--export-file"
+  elif "$INKSCAPE" --help | grep -e "--export-png" > /dev/null; then
+    EXPORT_FILE_OPTION="--export-png"
+  fi
+fi
+
 if [[ "$1" == "dark" ]]; then
   SRC_FILE="assets-dark.svg"
   ASSETS_DIR="assets-dark"
@@ -26,21 +36,14 @@ else
 fi
 
 echo "Rendering '$ASSETS_DIR/$i.png'"
+
 if [[ -n "${RENDER_SVG}" ]]; then
   # @TODO: remove --zoom when it will be fixed/implemented in resvg
   "$RENDER_SVG" --export-id "$i" \
                 --dpi ${DPI} \
                 --zoom ${ZOOM} \
                 "$SRC_FILE" "$ASSETS_DIR/$i.png"
-else
-  if "$INKSCAPE" --help | grep -e "--export-filename" > /dev/null; then
-    EXPORT_FILE_OPTION="--export-filename"
-  elif "$INKSCAPE" --help | grep -e "--export-file" > /dev/null; then
-    EXPORT_FILE_OPTION="--export-file"
-  elif "$INKSCAPE" --help | grep -e "--export-png" > /dev/null; then
-    EXPORT_FILE_OPTION="--export-png"
-  fi
-
+elif [[ -n "${INKSCAPE}" ]]; then
   "$INKSCAPE" --export-id="$i" \
               --export-id-only \
               --export-dpi=${DPI} \
